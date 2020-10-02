7b5db7d63ed113bdf80d8fe5897d4d06489a42f2
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-02 22:45:30 +0000

    Finished duck,hen,snake

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    //Ask if it has feathers
+    System.out.println("Does the animal have feathers?");
+    String featherAnswer = input.nextLine();
+    if(featherAnswer.equals("yes")){
+      //check if the animal swims
+System.out.println("Does the animal swim?");
+String animalSwim = input.nextLine();
+if(animalSwim.equals("yes")){
+  System.out.println("this is a duck");
+} else if(animalSwim.equals("no")){
+  System.out.println("This is a hen");
+}
+}else if(featherAnswer.equals("no")){
+  //see if it has legs
+  System.out.println("Does the animal have legs?");
+  String animalLegs = input.nextLine();
+  if(animalLegs.equals("yes")){
+    System.out.println("This is a lizard");
+  }else if(animalLegs.equals("no")){
+    System.out.println("This is a snake");
   }
 }
+    }
+    
+  }
+
